64da3f4543d8a4284a2b731b9159ab23721332ae
EDJ <jinende1997@gmail.com>
2018-05-26 20:36:16 +0000

    Core of Term Constructor is completed.

--- src/ITP.pver.js
+++ src/ITP.pver.js
@@ -1,34 +1,38 @@
 //@flow
-
+import * from ITP2
 // proof constructor : Command -> pttm
 // proof constructor only works at type-level
 type Dict<K, V> = Array<[K, V]>;
 
 
-let obeq = (a: Object, b:Object):boolean => (typeof a === typeof b) && (JSON.stringify(a) === JSON.stringify(b));
+const obeq = (a: Object, b:Object):boolean => (typeof a === typeof b) && (JSON.stringify(a) === JSON.stringify(b));
 
 //let lift_maybe = <D,C>(f: D => C): ((D | undefined) => (C | undefined)) => 
 //            (input : D | undefined) => {if (D !== undefined) {return f(D);} else {return undefined;} }
 
-let _add_to_dict = <K, V>(newterm : K, newtype : V, ctx: Dict<K,V>) : Dict<K,V> => {let r = ctx.slice(); r.push([newterm, newtype]); return r;}
-let _find_in_dict = <K,V>(pred: K => boolean, ctx : Dict<K,V>) : Option<V> => (x => {if(!x){return x[1];}else{return undefined;}})(ctx.filter(x => pred(x[0]))[0]);
-let _reverse_mapping = <K, V>(d : Dict<K,V>) : Dict<V,K> => d.map(x => [x[1], x[0]]);
+const _add_to_dict = <K, V>(newterm : K, newtype : V, ctx: Dict<K,V>) : Dict<K,V> => {let r = ctx.slice(); r.push([newterm, newtype]); return r;}
+const _find_in_dict = <K,V>(pred: K => boolean, ctx : Dict<K,V>) : Option<V> => 
+        (x_ => {const x = x_[x_.length - 1]; if(!x){return x[1];}else{return undefined;}})(ctx.filter(x => pred(x[0])));
+const _reverse_mapping = <K, V>(d : Dict<K,V>) : Dict<V,K> => d.map(x => [x[1], x[0]]);
 
-type ValOfCtx = pttm;
+type ValOfCtx = [pttm | "bottom" | false, pttm]; // definee & its type, if its definee is bottom, its primitive definition; if it is false, then it is in context
+
 type Context = Dict<number, ValOfCtx>;
-type DefContext = Dict<number, [pttm, pttm]>;
+const addCtx = _add_to_dict;
+const findinCtx = (ctx: Context, n:number) => _find_in_dict(j => n === j, ctx);
+//type DefContext = Dict<number, [pttm, pttm]>;
 //type GlobalContext = Context;
-type Judgement = [DefContext, Context, pttm];
-type Goal = [DefContext, Context, pttm];
+type Judgement = [Context, pttm];
+type Goal = [Context, pttm];
 type Goals = Array<Goal>
 type PartialGoals = Array<Goal | true>
 type NewContext = Context;
-type NewJudgement = [NewContext, pttm];
+type NewJudgement = pttm;
 type Commands = Array<Command>;
 type ArrayF<Domain, Codomain> = [number, Array<Domain> => Array<Codomain>]; // size of doman * function
 
 // homomorphism
-let connect = <D,C>(f : ArrayF<D,C>, g: ArrayF<D,C>) : ArrayF<D,C> => {
+const connect = <D,C>(f : ArrayF<D,C>, g: ArrayF<D,C>) : ArrayF<D,C> => {
     const f_ = (a : Array<D>) : Array<C> => f[1](a.slice(0, f[0]));
     const g_ = (a : Array<D>) : Array<C> => g[1](a.slice(f[0], f[0] + g[0]));
     return [f[0] + g[0], a => f_(a).concat(g_(a))];
@@ -37,26 +41,97 @@ let connect = <D,C>(f : ArrayF<D,C>, g: ArrayF<D,C>) : ArrayF<D,C> => {
 
 
 type Command =
-    {type : "intro", n : NewJudgement}
+    {type : "intro"}
     | {type : "apply", caller : NewJudgement, callee : NewJudgement}
     | {type : "check", term : pttm}
     | {type : "conv", newform : NewJudgement} // type level calculation
     | {type : "let", n : number, term : pttm} // local definition
     
-let combine = (gs : Array<[PartialGoals, ArrayF<pttm, pttm>]>) : [PartialGoals, ArrayF<pttm, pttm>] => {
+const combine = (gs : Array<[PartialGoals, ArrayF<pttm, pttm>]>) : [PartialGoals, ArrayF<pttm, pttm>] => {
     const goals = gs.reduce((x,y) => x[0].concat(y[0]));
     const fs = gs.reduce(connect);
     return [goals, fs];
 }
     
-let goaltransform = (cmd : Command, goal : Goal | true) : [PartialGoals, ArrayF<pttm, pttm>] => {
+const untyped_delta_conv_all = (ctx : Context, tm : pttm) : pttm => {
+    const utca = x => untyped_delta_conv_all(ctx, x);
+    if(tm.type === "lambda") {
+        return {type : "lambda", bind : tm.bind, iT : tm.iT, body : utca(tm.body)};
+    } else if(tm.type === "pi") {
+        return {type : "pi", bind : tm.bind, iT : tm.iT, body : utca(tm.body)};
+    } else if(tm.type === "apply") {
+        return {type : "apply", fun : utca(tm.fun), arg : utca(tm.arg)};
+    } else if(tm.type === "var") {
+        const replacement0 : ValOfContext = (findinCtx(ctx, tm.n));
+        if(replacement0[0] === "bottom") {
+            return tm;
+        } else if(replacement0[0] === false) {
+            return tm;
+        } else {
+            return utca(replacement0[0]);
+        }
+    } else {
+        return tm;
+    }
+}
+
+const untyped_beta_delta_conv_all = (ctx : Context, tm : pttm) : pttm => untyped_beta_conv(untyped_delta_conv_all(ctx, tm));
+
+const goaltransform = (warn : string => string; cmd : Command, goal_ : Goal | true) : [PartialGoals, ArrayF<pttm, pttm>] => {
+    const goal = goal_;
     if(goal === true) {
         return [[true], [1, x => x]];
+    }
+    // Now goal is not true
+    let ctx = goal[0];
+    let goal_ty = goal[1];
+    if(cmd.type === "intro") {
+        // check truely a function
+        if(goal_ty.type !== "pi") {warn("Intro failed."); return [[goal], x => x];}
+        return [[[addCtx(goal_ty.bind, [false, goal_ty.iT], ctx), goal_ty.body]],
+                x => [{type : "lambda", bind : goal_ty.bind, iT : goal_ty.iT, x[0]}] ];
+    } else if (cmd.type === "apply") {
+        const claimed_fty = cmd.caller;
+        const claimed_xty = cmd.callee;
+        const ctx_list = ctx.map(x => [x[0], x[1][1]]);
+        const type_of_claimed_f = has_type(ctx_list, claimed_fty);
+        const type_of_claimed_x = has_type(ctx_list, claimed_xty);
+        if(type_of_claimed_f === undefined || !obeq(type_of_claimed_f, TYPE_STAR) 
+            || type_of_claimed_x === undefined || !obeq(type_of_claimed_f, TYPE_STAR)) {warn("Apply failed. Type Inconsistent."); return [[goal], x => x];}
+        if(claimed_fty.type !== "pi") {warn("Apply failed. Type Inconsistent."); return [[goal], x => x];}
+        if(!obeq(claimed_fty.iT, claimed_xty)) {warn("Apply failed. Type Inconsistent."); return [[goal], x => x];}
+        return [
+                [[ctx, claimed_fty],
+                 [ctx, claimed_xty]] : PartialGoals,
+                 x => [{type: "apply", fun : x[0], arg: x[1]}];
+                ];
+    } else if (cmd.type === "check") {
+        const claimed_term = cmd.term;
+        const claimed_term_ty = has_type(ctx, claimed_term);
+        if(claimed_term_ty !== goal_ty) {warn("Check failed. Type Inconsistent."); return [[goal], x => x];}
+    } else if (cmd.type === "conv") {
+        const claimed_ty = cmd.newform;
+        // now beta, delta conversion
+        const ty_claimed_ty = has_type(ctx, claimed_ty);
+        if(ty_claimed_ty === undefined) {warn("Conversion failed. Types are not equivalent."); return [[goal], x => x];}
+        if(!obeq(untyped_beta_delta_conv_all(ctx, claimed_ty), untyped_beta_delta_conv_all(ctx, goal_ty))) {warn("Conversion failed. Types are not equivalent."); return [[goal], x => x];}
+        return [[ctx, claimed_ty], x => x];
+        
+    } else if (cmd.type === "let") {
+        const new_add_term = cmd.term;
+        const new_add_term_ty = has_type(ctx, new_add_term);
+        if(new_add_term_ty === undefined)) {warn("Local Define failed. Unsupported type"); return [[goal], x => x];}
+         return [[[addCtx(cmd.bind, [new_add_term, new_add_term_ty], ctx), goal_ty]],
+                x => [  {type: "apply",
+                            fun : {type : "lambda", bind : cmd.bind, iT : new_add_term_ty, x[0]},
+                            arg : new_add_term
+                        }
+                        ] ];
     }
     
 
 }
-let pfconstructor = (ncmd : (PartialGoals) => Commands, warn: string => string, currentGoals : PartialGoals) : [pttm] => {
+const pfconstructor = (ncmd : (PartialGoals) => Commands, warn: string => string, currentGoals : PartialGoals) : [pttm] => {
     let nextcmds_ = ncmd(currentGoals);
     while(nextcmds.length !== currentGoals.length) {
         warn("Error: Command number not enough");
--- src/ITP2.js
+++ src/ITP2.js
@@ -4,14 +4,14 @@
 type Dict<K, V> = Array<[K, V]>;
 
 
-let obeq = (a: Object, b:Object):boolean => (typeof a === typeof b) && (JSON.stringify(a) === JSON.stringify(b));
+const obeq = (a: Object, b:Object):boolean => (typeof a === typeof b) && (JSON.stringify(a) === JSON.stringify(b));
 
 //let lift_maybe = <D,C>(f: D => C): ((D | undefined) => (C | undefined)) => 
 //            (input : D | undefined) => {if (D !== undefined) {return f(D);} else {return undefined;} }
 
-let _add_to_dict = <K, V>(newterm : K, newtype : V, ctx: Dict<K,V>) : Dict<K,V> => {let r = ctx.slice(); r.push([newterm, newtype]); return r;}
-let _find_in_dict = <K,V>(pred: K => boolean, ctx : Dict<K,V>) : Option<V> => (x => {if(!x){return x[1];}else{return undefined;}})(ctx.filter(x => pred(x[0]))[0]);
-let _reverse_mapping = <K, V>(d : Dict<K,V>) : Dict<V,K> => d.map(x => [x[1], x[0]]);
+const _add_to_dict = <K, V>(newterm : K, newtype : V, ctx: Dict<K,V>) : Dict<K,V> => {let r = ctx.slice(); r.push([newterm, newtype]); return r;}
+const _find_in_dict = <K,V>(pred: K => boolean, ctx : Dict<K,V>) : Option<V> => (x => {if(!x){return x[1];}else{return undefined;}})(ctx.filter(x => pred(x[0]))[0]);
+const _reverse_mapping = <K, V>(d : Dict<K,V>) : Dict<V,K> => d.map(x => [x[1], x[0]]);
 
 
 
@@ -25,12 +25,15 @@ type pttm =
     | {type : "pi", bind : number, iT : pttm, body : pttm}
     | {type : "var", n : number};
     
+const TYPE_STAR : pttm = {type : "U0"};
+const TYPE_SQUARE : pttm = {type : "U1"};
+    
 type ty = pttm;
 type Context = Dict<number, ty>;
 type Option<T> = T | typeof undefined;
 
 
-let subst = (exp : pttm, from : number, to : pttm) : pttm => {
+const subst = (exp : pttm, from : number, to : pttm) : pttm => {
     let sb = (subexp : pttm) : pttm => subst(subexp, from, to);
     if(((exp.type === "lambda")) && exp.bind !== from) {
         return {type: "lambda", bind : exp.bind, iT : exp.iT, body : sb(exp.body)};
@@ -45,7 +48,7 @@ let subst = (exp : pttm, from : number, to : pttm) : pttm => {
 }
 
 
-let untyped_beta_conversion = (tm : pttm) : pttm => {
+const untyped_beta_conversion = (tm : pttm) : pttm => {
     if(tm.type === "lambda") {
         return {type : "lambda", bind : tm.bind, iT : tm.iT, body : untyped_beta_conversion(tm.body)};
     } else if(tm.type === "pi") {
@@ -62,7 +65,7 @@ let untyped_beta_conversion = (tm : pttm) : pttm => {
 
 // One specification: beta_conversion(has_type(..)) === has_type(..)
 // Another : beta_conversion(find_in_dict(..)) == find_in_dict(..)
-let has_type = (ctx : Context, tmm : pttm) : Option<ty> => {
+const has_type = (ctx : Context, tmm : pttm) : Option<ty> => {
     // I don't need to hard-code them
     const tm = tmm;
     if(tm.type === "U1") {
