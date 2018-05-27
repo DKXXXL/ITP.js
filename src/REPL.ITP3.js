60a0a51765c1aba64dec6299290e319300f54a51
EDJ <jinende1997@gmail.com>
2018-05-27 17:21:16 -0400

    flow on win10 seems not working.

--- src/REPL.ITP3.js
+++ src/REPL.ITP3.js
@@ -12,14 +12,29 @@ type Actic = PartialGoals => Commands;
 type Generator<X> = () => Option<X> ;
 type TContext = Dict<number, Tactic>;
 
+// MEDIUM LEVEL : META TACTIC
+// A Tactic is a instruction that can be interpreted into (A function from PartialGoals to Commands := Actic)
+
 type Tactic = 
-    {type : "cmd", t : Commands}
+    {type : "cmds", t : Commands}
     | {type : "seq", t0 : Tactic, t1 : Tactic}
     | {type : "let", name : number, bind : Tactic, body : Tactic}
     | {type : "abs", name : number, body : Tactic}
     | {type : "call"}
 
-const emptyGen= <X>(s:()):Option<X> => undefined;
+// TContext -> String
+const prettyprintTac = (tctx : TContext) : string => {
+
+}
+
+// Array -> Generator
+const listGen= <X>(l : Array<X>) : Generator<X> => {
+    let index = 0;
+    return () => {
+        index = index + 1;
+        return l[index];
+    }
+}
 
 const concat = <X>(f : Generator<X>, g : Generator<X>):Generator<X> => {
                             let flag = true;
@@ -55,21 +70,30 @@ const joinGen = <X>(f : Generator<Generator<X>>) : Generator<X> => {
 
 const mapGen = <X, Y>(fmap : X => Y, gen : Generator<X>) : Generator<Y> => (() => fmap(gen()));
 
+// The interpreter of Tactic
 const tacticIntp = (tctx : TContext, tac : Tactic) : Generator<Actic> => {
-    if(tac.type === "seq"){
-        return concat()
+    if(tac.type === "cmds"){
+        return listGen([s => tac.t]);
+    } else if(tac.type === "seq") {
+        const tip = t => tacticIntp(tctx, t);
+        return concat(tip(tac.t0), tip(tac.t1));
+    } else if(tac.type === "let") {
+        
+        return tacticIntp(_add_in_dict(tac.name, tac.bind, tctx), )
     }
 };
 
 type stdIO = {i : Input<Tactic>, iI : Input<INSTRUCTION>, o : Output, e : Error};
 
-type Input<K> = string => <K>;
+type Input<K> = string => K;
 type Output = string => string;
 type Error = string => typeof undefined;
 
 const inputAsGen = (i : Input) : Generator<Tactic> => (x => i(""));
 
 const prettyprint = (pg : PartialGoals) : string => 
+
+
 
 const interaction = (ioe : stdIO, tctx : TContext) : PartialGoals => Commands => {
     const tacticInput : Generator<Actic> = joinGen(mapGen(y => tacticIntp(tctx, y), inputAsGen(ioe.i)));
@@ -81,7 +105,7 @@ const interaction = (ioe : stdIO, tctx : TContext) : PartialGoals => Commands =>
 const PFCONSOLE = (ioe : stdIO, tctx : TContext, dctx : DefinitionList, newty : pttm) : pttm => 
     pfconstructor(interaction(ioe, tctx), ioe.e, [[dctx, newty]]);
 
-
+// UPPER LEVEL : CONSOLE
 
 type INSTRUCTION = 
     {type : "addDef", name : number, ty : pttm}
@@ -102,16 +126,27 @@ const CONSOLE = (ioe : stdIO) : typeof undefined => {
         } else if(input.type === "addDef") {
             // Into Proof Mode
             const tm = PFCONSOLE(ioe, AllTactics, AllDefinitions, input.ty);
+            if(!newtermChecker(AllDefinitions, input.name, tm, input.ty)) {ioe.e("Define Failed."); continue;}
             AllDefinitions.push([input.name, tm]);
         } else if(input.type === "addTactics") {
-            
-        }
+            AllTactics.push([input.name, input.tac]);
+        } else if(input.type === "printScript") {
+            input.outMethod(ProofScript);
+        } else if(input.type === "printDef") {
+            if(!pfChecker(AllDefinitions)) {ioe.e("Unexpected Internal Error. Cannot output definition."); continue;}
+            input.outMethod(AllDefinitions);
+        } else if(input.type === "printTacs"){
+            ioe.o(prettyprintTac(AllTactics));
+        } 
     }
 
     return undefined;
 }
 
 
+
+// Intended to make it monadic form
+// I think if I did that, I would be an idiot.
 
 //
 // class StateTransition<S, R>{
